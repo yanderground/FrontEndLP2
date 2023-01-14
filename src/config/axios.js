8b7122ad63c67b8486bcad1cbb2f44e4058dadb4
MarcioMAlmeida <marcioa.marcioa@gmail.com>
2023-01-14 11:45:21 -0300

    configurando apis fake

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,3 +1,9 @@
 export const BASE_URL =
-  'https://my-json-server.typicode.com/MarcioMAlmeida/fake-api-lp2'
+  'https://my-json-server.typicode.com/MarcioMAlmeida/fake-api-lp2';
   //'https://my-json-server.typicode.com/marcoaparaujo/jsonfake';
+
+  export const BASE_URL_2 =
+  'https://my-json-server.typicode.com/MarcioMAlmeida/fake-api-lp2-2';
+
+  export const BASE_URL_3 =
+  'https://my-json-server.typicode.com/MarcioMAlmeida/fake-api-lp2-3';
